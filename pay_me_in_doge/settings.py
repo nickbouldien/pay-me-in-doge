cf68a1dc74d8fbc76eea7619d02bf1d70c2e249c
nick bouldien <nick.bouldien@gmail.com>
2019-04-06 14:45:35 -0500

    #7 create users app and register template

--- pay_me_in_doge/settings.py
+++ pay_me_in_doge/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "board.apps.BoardConfig",
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
