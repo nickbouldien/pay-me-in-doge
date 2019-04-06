4646f6e4bdbad34217d682c0ad38e1efbfa481b9
nick bouldien <nick.bouldien@gmail.com>
2019-04-05 22:11:36 -0500

    add first migration for Site model, change formatting of dates in home.html, fix formatting of html files after disabling prettier & html

--- board/models.py
+++ board/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Site(UUIDModel):
+    name = models.CharField(max_length=50)
+    poster = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    link = models.CharField(max_length=50)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    upvotes = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
+    def __repr__(self):
+        return "Site('{}', '{}', '{}')".format(self.name, self.link, self.id)
+
+    def __str__(self):
+        return "'{}': '{}' - '{}'".format(self.name, self.link, self.description)
+
--- board/views.py
+++ board/views.py
@@ -1,27 +1,9 @@
 from django.shortcuts import render
-
-mock_sites = [
-    {
-        "link": "http://dogeiscool.com",
-        "name": "dogeiscool",
-        "poster": "doge4life",
-        "date_posted": "November 29, 2018",
-        "upvotes": 3,
-        "description": "marketplace for doge things",
-    },
-    {
-        "link": "http://dogerulez.all",
-        "name": "doge rulez",
-        "poster": "dogegod",
-        "date_posted": "November 23, 2018",
-        "upvotes": 7,
-        "description": "the place where you can buy things",
-    },
-]
+from .models import Site
 
 
 def home(request):
-    context = {"sites": mock_sites}
+    context = {"sites": Site.objects.all()}
     return render(request, "board/home.html", context)
 
 
