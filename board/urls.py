89a2b6079611ef6260a77653258e63d108c7f429
nick bouldien <nick.bouldien@gmail.com>
2019-04-07 16:29:17 -0500

    #7 use class based views to add list and detail views for 'sites'

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import SiteDetailView, SiteListView
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="board-home"),
+    path("", SiteListView.as_view(), name="board-home"),
+    path("site/<uuid:pk>", SiteDetailView.as_view(), name="site-detail"),
     path("about/", views.about, name="board-about"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from .models import Site
 
 
 def home(request):
-    context = {"sites": Site.objects.all().order_by("-upvotes")}
+    context = {"sites": Site.objects.all().order_by("upvotes")}
     return render(request, "board/home.html", context)
+
+
+class SiteListView(ListView):
+    model = Site
+    template_name = "board/home.html"
+    context_object_name = "sites"
+    ordering = ["-upvotes"]
+
+
+class SiteDetailView(DetailView):
+    model = Site
 
 
 def about(request):
