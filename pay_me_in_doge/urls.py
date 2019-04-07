efd36a1b7dd5ad0cd2f79ab10bbcc37ccd073ea8
nick bouldien <nick.bouldien@gmail.com>
2019-04-07 00:42:39 -0500

    #7 - add images to profiles (are saved in /media dir with the default image being default.png

--- pay_me_in_doge/urls.py
+++ pay_me_in_doge/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -20,3 +22,6 @@ urlpatterns = [
     path("", include("board.urls")),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from board.models import UUIDModel
 
-# Create your models here.
+
+class Profile(UUIDModel):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="default.png", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
