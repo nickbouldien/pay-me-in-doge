54c0db1066f6fceb93de146d68d60c06cf7fd8fe
nick bouldien <nick.bouldien@gmail.com>
2019-04-07 08:39:04 -0500

    #7 let user upload profile pic - WIP. in future will upload to s3 or similar

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from board.models import UUIDModel
+from PIL import Image
 
 
 class Profile(UUIDModel):
@@ -11,3 +12,12 @@ class Profile(UUIDModel):
     def __str__(self):
         return f"{self.user.username} Profile"
 
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
 
 
 def register(request):
@@ -21,5 +22,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
+
+    else:
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {"p_form": p_form}
+
+    return render(request, "users/profile.html", context)
 
