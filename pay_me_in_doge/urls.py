39fac109aeb487345273b2d678d4dece2fe5cbb0
nick bouldien <nick.bouldien@gmail.com>
2019-04-09 22:16:56 -0500

    #15 finish password reset flow

--- pay_me_in_doge/urls.py
+++ pay_me_in_doge/urls.py
@@ -17,6 +17,32 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
     ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
     path("profile/", user_views.profile, name="profile"),
     # path("profile/<uuid:pk>/", user_views.public_profile, name="public-profile"),
     path("register/", user_views.register, name="register"),
