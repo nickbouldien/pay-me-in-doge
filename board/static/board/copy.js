4a9cd785a04c3b55d5d541bec12024cb6c4f7fe0
nick <nick.bouldien@gmail.com>
2019-04-23 21:54:26 -0500

    #20 add clipboard.js, add clipboard svg and use on user_sites.html

---
+++ board/static/board/copy.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  var clipboard = new ClipboardJS(".clipboard");
+
+  clipboard.on("success", function(e) {
+    console.info("Action:", e.action);
+    console.info("Text:", e.text);
+    console.info("Trigger:", e.trigger);
+
+    e.clearSelection();
+  });
+
+  clipboard.on("error", function(e) {
+    console.error("Action:", e.action);
+    console.error("Trigger:", e.trigger);
+  });
+});
